bdb144a29aa40fd6a44666b1c19733350e2071b6
Yu Jung Eun <0hh.0553@gmail.com>
2022-08-02 14:37:31 +0900

    list에서 선택한 목록 출력

---
+++ And0802_06_Cuslist/app/src/main/java/com/cookandroid/and0802_06_cuslist/MainActivity.java
@@ -0,0 +1,15 @@
+package com.cookandroid.and0802_06_cuslist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
